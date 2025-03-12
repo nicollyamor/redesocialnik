c7b290bfd9a6137876f862cb42d7fec1f492e5a7
nicollyamor <nicolly.reglly@escola.pr.gov.br>
2025-03-12 19:47:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let audios = [
-    {caminho:'matuto.mp3', legenda:'Matuto de verdade'},
+    {caminho:'Jorge & Mateus - Os Anjos Cantam (LyricVideo) [Álbum Os Anjos Cantam].mp3'},
     {caminho:'Arash.mp3', legenda:'Arash'},
     {caminho:'Paula Fernandes.mp3', legenda:'Paula Fernandes'},
     {caminho:'Hayit.mp3', legenda:'Hayit'},
